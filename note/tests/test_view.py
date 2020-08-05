5f16bc6ca916c8d8e9a6c5f14de22e530fdd17ba
atoyegbe <herdeyemie@gmail.com>
2020-08-04 18:50:03 -0700

    test:white_check_mark:

--- note/tests/test_view.py
+++ note/tests/test_view.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from note.models import Note
+from note.forms import NoteForm
 from datetime import datetime
 from django.forms.models import model_to_dict
 
@@ -9,11 +10,11 @@ class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
         self.home_url = reverse('home')
-        self.list_details = reverse('note', args=[4])
+        self.list_details = reverse('note', kwargs={'pk': 4})
+        self.addNote_url = reverse('add_note')
         self.note_one = Note.objects.create(
             title='first title',
             text='My first view test',
-            date_created=datetime.now()
         )
         
     
@@ -23,11 +24,45 @@ class TestViews(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
     
-    def test_note_details_GET(self):
-        data = model_to_dict(self.note_one)
-        response = self.client.get(self.list_details, data=data)
+    # def test_note_details_GET(self):
+    #     """Retrieve Note """
+    #     note_dix = Note.objects.create(
+    #         title='first title',
+    #         text='My first view test',
+    #     )
+    #     url = reverse('note', kwargs={'pk': 1})
         
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'note.html')
+    #     data = model_to_dict(note_dix)
+    #     response = self.client.get(url)
+        
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'note.html')
         
    
+    def test_note_details_POST(self):
+        """ Adding Notes """
+        data = model_to_dict(self.note_one)
+        response = self.client.post(self.addNote_url, data=data)
+        
+        self.assertEquals(response.status_code, 302)
+        self.assertEqual(self.note_one.title, 'first title')
+        self.assertRedirects(response, self.home_url)
+        
+    
+    def test_edit_note_details_POST(self):
+         """ Testing Edit note """
+        
+         form_data = {
+            'title':'first title',
+            'text':'My first view test update',
+         }
+         
+         url = reverse('edit_note', kwargs={'pk': 4})
+         add_form = NoteForm(data= form_data, instance= self.note_one)
+         add_form.save()
+         response = self.client.post(url, form_data)
+         self.assertEquals(response.status_code, 302)
+         self.assertEquals(self.note_one.text, 'My first view test update')
+        
+    
+        
